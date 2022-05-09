ffd0b6c768f8e9a20edf5987feca9a72ee60e72f
Kenneth Barkdoll <kbarkdo1@swarthmore.edu>
2022-05-09 11:01:59 -0400

    outlining 1

--- Sim1.c
+++ Sim1.c
@@ -1,1 +1,23 @@
 // Welcome to the Machine
+
+
+
+// initialize all variables
+// make neuron connections
+// make neuron voltages
+// make image input
+
+// iterate through all times steps
+// iterate for 10 by 0.01
+
+// calculate voltage steps
+
+// take voltage steps
+
+// check for spiked Neurons
+
+// handle spikes
+
+// handles future spikes
+
+// save data

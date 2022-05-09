a60bd54993dab4ee5067e8a9ba451b8a3b1a4aa6
Kenneth Barkdoll <kbarkdo1@swarthmore.edu>
2022-05-09 14:50:37 -0400

    implementing spike catch 1

--- Sim1.cpp
+++ Sim1.cpp
@@ -57,10 +57,11 @@ int main() {
   for(float t=0; t<10; t = t+step) {
 
     // for every neuron
-    int* k1 = new int [1000];
-    int* k2 = new int [1000];
+    float* k1 = new float [1000];
+    float* k2 = new float [1000];
+    float* delta = new float [1000];
     int fired[1000];
-    vector need_firing;
+    vector<int> mybabies; // list of neurons that need firing
 
     for(int n=0; n < 1000; n++) {
       // calculate voltage steps
@@ -71,8 +72,21 @@ int main() {
       float img_in = image[n];
       printf("img_in = %2.4f\n", img_in);
       // calculate neighboring inputs
-      float neighbor_in;
 
+      k1[n] = volt_diff + img_in;
+
+      float volt_diff_2 = -(voltages[n]+k1[n]);
+      printf("voltage_diff = %2.4f\n", volt_diff);
+      // calculate image input
+      float img_in_2 = image[n];
+      printf("img_in = %2.4f\n", img_in);
+      // calculate neighboring inputs
+
+      k2[n] = volt_diff_2 + img_in_2;
+
+      delta[n] = 0.5 * (k1[n] + k2[n]);
+
+      /*
       int target = n;
       for(int from = 0; from < (1000); from++) {
         neighbor_in = neighbor_in + connect[target * 1000 + from]
@@ -85,26 +99,81 @@ int main() {
       neighbor_in = neighbor_in * s_N;
       printf("neighbor_in = %2.10f\n", neighbor_in);
       // sleep(5);
+      */
+      // check overrun - note k1 is just the forward step
 
-      float slope1 = volt_diff + img_in + neighbor_in;
-      float step_1 = step +
-
-      // calculate the second steps
-
-      // take average voltage step w/ heun
-
-      // check overrun to 1
-      // if 1
-        // record as spiked (hashtable?)
-        // note connections
-        // for each connection
-          // update with new input
-          // check overrun to 1
-          // if 1
-            // this seems like a great place for recursion, carefully
-            // or a list
+      if (voltages[n] + delta[n] >= 1) {
+        voltages[n] = 0;
+        fired[n] = 1;
+        int from = n;
+        for(int target=0; target < 1000; target++) {
+          if (connect[target * 1000 + from]) {
+            mybabies.push_back(target);
+          }
+        }
       }
 
+      // iterating through mybabies - note k1 is just the forward step
+
+    }
+
+    for(int i=0; i< mybabies.size(); i++) {
+      // recalculating action potential
+      if (!fired[mybabies[i]]) { // i f its already fired
+        continue;
+      }
+      int here = mybabies[i];
+      float neighbor_in = 0;
+      int target = here;
+      for(int from = 0; from < (1000); from++) {
+        // note this is not how to calcualte neighbors if only action
+        // potential neighbors count
+        neighbor_in = neighbor_in + connect[target * 1000 + from]
+          * fired[from];
+      }
+      int s = 1;
+      int nn = 1000;
+      float s_N = s / float(nn);
+      neighbor_in = neighbor_in * s_N;
+      delta[here] = delta[here] + neighbor_in; // note no RK2 on this
+      if (voltages[here] + delta[here] >= 1) {
+        voltages[here] = 0;
+        fired[here] = 1;
+        int from = here;
+        for(int target=0; target < 1000; target++) {
+          if (connect[target * 1000 + from]) {
+            mybabies.push_back(target);
+          }
+        }
+      }
+    }
+
+    /*
+
+    // calculate the second steps
+    
+    for rec
+    int s = 1;
+    int nn = 1000;
+    float s_N = s / float(nn);
+    neighbor_in = neighbor_in * s_N;
+    k1[rec] = k1[rec] + neighbor_in;
+    if k1[rec]>=1 {
+
+    }
+    */
+    // take average voltage step w/ heun
+
+    // check overrun to 1
+    // if 1
+      // record as spiked (hashtable?)
+      // note connections
+      // for each connection
+        // update with new input
+        // check overrun to 1
+        // if 1
+          // this seems like a great place for recursion, carefully
+          // or a list
       // save data
       // save which neurons fired
       // save selective single neuron voltages

84df1147c0d7f94b2e6a79ef4cfce3b4b278725e
Kenneth Barkdoll <kbarkdo1@swarthmore.edu>
2022-05-11 16:58:04 -0400

    fixed exclamation mark

--- Sim1.cpp
+++ Sim1.cpp
@@ -28,7 +28,7 @@ int main() {
   // initialize all variables
   // make neuron connections
   int neur_num = 1000;
-  float time = 10;
+  float cycles = 10.00;
   int* connect = new int [neur_num*neur_num];
 
   float check_ratio=0;
@@ -69,6 +69,11 @@ int main() {
     // printf(" %2.4f", voltages[i]);
   }
 
+  int* fired = new int [neur_num];
+  for(int i =0; i< neur_num; i++) {
+    fired[i] = 0;
+  }
+
 
   printf("%2.4f, %2.4f, \n", voltages[0], image[0]);
   printf("%2.4f, %2.4f, \n", voltages[915], image[915]);
@@ -80,17 +85,15 @@ int main() {
 
   float step = 0.01;
   float f = 1;
+  printf("all values \n");
   //float B = 1;
-  for(float t=0; t<time; t = t+step) {
-    // printf("Time = %2.2f", t);
+  for(float t=0; t<cycles; t = t+step) {
+    //printf("Time = %2.2f", t);
     // for every neuron
     float* k1 = new float [neur_num];
     float* k2 = new float [neur_num];
     float* delta = new float [neur_num];
-    int fired[neur_num];
-    for(int i =0; i< neur_num; i++) {
-      fired[i] = 0;
-    }
+
     vector<int> mybabies; // list of neurons that need firing
 
     for(int n=0; n < neur_num; n++) {
@@ -145,7 +148,7 @@ int main() {
 
       if (voltages[n] + delta[n] >= 1) {
         //printf("We have a firing!");
-        voltages[n] = 0;
+        //voltages[n] = 0;
         fired[n] = 1;
         int from = n;
         for(int target=0; target < neur_num; target++) {
@@ -164,6 +167,15 @@ int main() {
 
     for(int i=0; i< neur_num; i++) {
       vec_arr[i].push_back(fired[i]);
+      /*if (i==915) {
+        printf("915: %d %d %2.6f %2.6f \n", fired[i], vec_arr[i][vec_arr[i].size()-1], voltages[i], t );
+      }*/
+      /*if (i==915) {
+        printf("915: %d %d   ", fired[i], vec_arr[i][vec_arr[i].size()-1]);
+      }*/
+    }
+    for(int i=0; i< neur_num; i++) {
+
       if (fired[i]==0) {
         float neighbor_in = 0;
         int target = i;
@@ -178,15 +190,17 @@ int main() {
         float s_N = s / float(nn);
         neighbor_in = neighbor_in * s_N;
         if (voltages[i]+delta[i]+neighbor_in >= 1) {
-          //printf("unfired overrun, %d, %2.6f\n", i, voltages[i]+delta[i]+neighbor_in);
+          printf("unfired overrun, %d, %2.6f, %2.6f\n", i, voltages[i]+delta[i], voltages[i]+delta[i]+neighbor_in);
         } else {
           voltages[i] = voltages[i]+delta[i]+neighbor_in;
         }
       } else if (fired[i]==1) {
+
+        voltages[i]=0;
         fired[i]=0;
-        if (voltages[i] != 0) {
-          printf("error: fired neuron has nonzero voltage");
-        }
+        //if (voltages[i] != 0) {
+          //printf("error: fired neuron has nonzero voltage");
+        //}
       }
     }
     /*
@@ -232,15 +246,25 @@ int main() {
 
   }
   */
+  ofstream n915;
+  n915.open ("n915_fire_track.txt");
+  for(int j=0; j< cycles/0.01; j++) {
+    printf("%d\n", vec_arr[915][j]);
+    n915 << vec_arr[915][j];
+  }
+  n915.close();
+
   ofstream raster;
   raster.open ("spikes.txt");
   ofstream times;
   times.open ("times.txt");
+
   for(int i=0; i<neur_num; i+=1) {
-    for(float j=0; j < time; j+=0.01) {
+    for(int j=0; j < cycles/0.01; j++) {
       if (vec_arr[i][j]) {
-        raster << to_string(i) << endl;
-        times << to_string(j) << endl;
+        raster << i << endl;
+        times << float(j)*0.01 << endl;
+        // printf("cycle: %2.6f\n", j);
       }
     }
   }
@@ -249,14 +273,14 @@ int main() {
 
 
   ofstream volt_file;
-  volt_file.open ("outputvecs/n0_volts.txt");
-  for(int j=0; j< time/0.01; j++) {
+  volt_file.open ("outputvecs/n0_volts.txt" );
+  for(int j=0; j< cycles/0.01; j++) {
     volt_file << n0_volts[j] << endl;
   }
   volt_file.close();
   ofstream volt_file2;
   volt_file2.open ("outputvecs/n915_volts.txt");
-  for(int j=0; j< time/0.01; j++) {
+  for(int j=0; j< cycles/0.01; j++) {
     volt_file2 << n915_volts[j] << endl;
   }
   volt_file2.close();
@@ -265,15 +289,28 @@ int main() {
 
 
 void chase_spikes(vector<int> to_check, float* voltages, float* delta, int* fired, int* connect, int neur_num) {
-  // printf("chasing_spikes with %zu ", to_check.size());
+  // printf("chasing_spikes with %zu \n", to_check.size());
   // printf("voltage 0: %2.4f ", voltages[915]);
+  /*int connect_sum=0;
+  for(int i=0;i<1000*1000;i++) {
+    connect_sum += connect[i];
+  }
+  printf("connect sum: %d ", connect_sum);*/
   vector<int> mybabies;
+  int num_fi = 0;
   if (to_check.size()< 1) {
     return;
   }
+  /*
+  for (int i=0; i<1000; i++) {
+    printf("%d %2.4f  ", i, voltages[i]);
+  }
+  */
   for(int i=0; i<to_check.size(); i++) {
+    // printf("child: %d %2.6f %2.6f \n", to_check[i], voltages[to_check[i]], voltages[to_check[i]] + delta[to_check[i]]);
     // recalculating action potential
-    if (!fired[to_check[i]]) { // i f its already fired
+    if (fired[to_check[i]]) { // i f its already fired
+      num_fi++;
       continue;
     }
     int here = to_check[i];
@@ -296,6 +333,7 @@ void chase_spikes(vector<int> to_check, float* voltages, float* delta, int* fire
     int nn = neur_num;
     float s_N = s / float(nn);
     neighbor_in = neighbor_in * s_N;
+
     // printf("neighbors_in: %2.4f", neighbor_in);
     /* wait this doesn't work. Right now if it has five neurons firing,
     it might count up three the first time, then all five the second time,
@@ -320,24 +358,35 @@ void chase_spikes(vector<int> to_check, float* voltages, float* delta, int* fire
     // delta[here] = delta[here] + neighbor_in; // note no RK2 on this
     // printf("future voltage: %d %2.4f, ", here, voltages[here] + delta[here] + neighbor_in);
 
-    printf("child: %2.4f ", (1-voltages[here]-delta[here]) - neighbor_in);
+    // printf("child: %d %2.4f %2.4f ", here, voltages[here], (1-voltages[here]-delta[here]) - neighbor_in);
+    /*
     if (((1-voltages[here]-delta[here]) - neighbor_in) <= 0) {
       printf("CHECKCHEKC ");
     }
+    */
+    //printf("Child %d: %2.6f, %2.6f, %2.6f\n", here, voltages[here] + delta[here], neighbor_in, voltages[here] + delta[here] + neighbor_in);
+    /*if (here==893) {
+      printf("expo 893: %d %2.6f %2.6f \n", here, voltages[here] + delta[here], voltages[here] + delta[here] + neighbor_in );
+    }*/
     if (voltages[here] + delta[here] + neighbor_in >= 1) {
-      //  printf("<-%d spiked \n", here);
-      printf("child spike");
-      voltages[here] = 0;
-      fired[here] = 1;
-      int from = here;
-      // push children for analysis
-      for(int target=0; target < neur_num; target++) {
-        if (connect[target * neur_num + from]) {
-          mybabies.push_back(target);
+      if (fired[here]!=1) {
+        //  printf("<-%d spiked \n", here);
+        //printf("child spike");
+        // voltages[here] = 0;
+        fired[here] = 1;
+        int from = here;
+        // push children for analysis
+        for(int target=0; target < neur_num; target++) {
+          if (connect[target * neur_num + from]) {
+            mybabies.push_back(target);
+          }
         }
+
       }
+
     }
   }
+  // printf("num_fi: %d ", num_fi);
   chase_spikes(mybabies, voltages, delta, fired, connect, neur_num);
   return;
 }
